43faa2c9f60fe8a9ace09d17c77aadc425c94587
Liovic031 <101591150+Liovic031@users.noreply.github.com>
2026-03-13 18:48:49 +0100

    pt1

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const IME_APLIKACIJE = 'Event Booking APP'
+
+export const RouteNames = {
+    HOME: '/',
+    DOGADJAJI: '/dogadjaji' 
+    // ovo je ruta
+}
---
+++ src/pages/dogadjaji/DogadjajPregled.jsx
@@ -0,0 +1,8 @@
+
+export default function DogadjajPregled(){
+    return(
+        <>
+            Ovdje će se vidjeti dogadjaji
+        </>
+    )
+}
