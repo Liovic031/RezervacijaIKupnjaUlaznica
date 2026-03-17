41cad57228c4195362706045ed7e186a573238bc
Liovic031 <101591150+Liovic031@users.noreply.github.com>
2026-03-17 16:27:14 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
 import Home from './pages/Home'
 import { RouteNames } from './constants'
 import DogadjajPregled from './pages/dogadjaji/DogadjajPregled'
+import { Container } from 'react-bootstrap'
+import Izbornik from './components/Izbornik'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 function App() {
 
 
   return (
-    <>
+    <Container>
+      <Izbornik />
       <Routes>
         <Route path={RouteNames.HOME} element={<Home />} />
         <Route path={RouteNames.DOGADJAJI} element={<DogadjajPregled />} />
       </Routes>
       <hr />
       &copy; Edunova
-    </>
+    </Container>
   )
 }
 
---
+++ src/components/Izbornik.jsx
@@ -0,0 +1,21 @@
+import { Container, Nav, Navbar } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+import { IME_APLIKACIJE, RouteNames } from "../constants"
+export default function Izbornik() {
+
+    const navigate = useNavigate()
+
+    return (
+
+        <Navbar bg="dark" data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand onClick={() => navigate(RouteNames.HOME)}>{IME_APLIKACIJE}</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link onClick={() => navigate(RouteNames.HOME)}>Home</Nav.Link>
+                    <Nav.Link onClick={() => navigate(RouteNames.DOGADJAJI)}>Događaji</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+
+    )
+}
