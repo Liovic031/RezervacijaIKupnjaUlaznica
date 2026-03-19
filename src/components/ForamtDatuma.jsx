0dd2e84e35d3355ea1ea106f245c0b76ab6c1b9a
Liovic031 <101591150+Liovic031@users.noreply.github.com>
2026-03-19 18:06:25 +0100

    Tablični prikaz

---
+++ src/components/ForamtDatuma.jsx
@@ -0,0 +1,14 @@
+export default function FormatDatuma({datum}){
+    if(!datum){
+        return '-';
+    }
+    const d = new Date(datum);
+    if(isNaN(d.getTime())){
+        return '-';
+    }
+    return Intl.DateTimeFormat('hr-HR',{
+        day:'2-digit',
+        month:'2-digit',
+        year:'numeric'
+    }).format(d) + (datum.includes('T') ? '' : '.')
+}
--- src/pages/dogadjaji/DogadjajPregled.jsx
+++ src/pages/dogadjaji/DogadjajPregled.jsx
@@ -1,30 +1,62 @@
 import { useEffect, useState } from "react"
 import DogadjajService from "../../services/dogadjaji/DogadjajService"
+import { Table } from "react-bootstrap"
+import { NumericFormat } from "react-number-format"
+import FormatDatuma from "../../components/ForamtDatuma"
 
 export default function DogadjajPregled() {
 
-
+    //dohvaćanje podataka
     const [dogadjaji, setDogadjaji] = useState([])
-
-
     async function ucitajDogadjaje() {
         const odgovor = await DogadjajService.get()
         setDogadjaji(odgovor.data)
 
     }
-
     useEffect(() => {
         ucitajDogadjaje()
     }, [])
 
+
+
     return (
         <>
-            <ul>
-                {dogadjaji && dogadjaji.map((dogadjaj) => (
-                    <li key={dogadjaj.sifra}>{dogadjaj.naziv}</li>
-                ))
-                }
-            </ul>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Naziv</th>
+                        <th>Lokacija</th>
+                        <th>Datum</th>
+                        <th>Slobodna mjesta</th>
+                        <th>Ukupno mjesta</th>
+                        <th>Cijena</th>
+                        <th>Aktivan</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {dogadjaji && dogadjaji.map((dogadjaj)=>(
+                        <tr>
+                            <td>{dogadjaj.naziv}</td>
+                            <td>{dogadjaj.lokacija}</td>
+                            <td><FormatDatuma datum={dogadjaj.datumOdrzavanja} /></td>
+                            <td>{dogadjaj.dostupnoMjesta}</td>
+                            <td>{dogadjaj.brojMjesta}</td>
+                            <td>
+                                <NumericFormat 
+                                value={dogadjaj.cijena}
+                                displayType={'text'}
+                                thousandSeparator='.'
+                                decimalSeparator=','
+                                suffix={' €'}
+                                decimalScale={2}
+                                fixedDecimalScale
+                                />
+                            </td>
+                            <td>{dogadjaj.aktivan ? 'Da' : 'Ne'}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </>
     )
 }
