4ec5df8657106ebd931c0598c5ded27cf7ae1d85
Liovic031 <101591150+Liovic031@users.noreply.github.com>
2026-03-31 18:45:56 +0200

    animacija i dev pozadina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,32 @@
+import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { IME_APLIKACIJE } from "../constants";
-import events from "../assets/events.png"
+import events from "../assets/events.png";
+import ticketAnimation from "../assets/animations/ticketblue.lottie";
 
-export default function Home(){
-    return(
+export default function Home() {
+  return (
     <>
+      <h1 style={{ textAlign: "center" }}>
+        Dobrodošli na {IME_APLIKACIJE}
+      </h1>
 
-        <h1>Dobrodošli na {IME_APLIKACIJE}</h1>
-        <div>
-            <img src={events} alt="events" style={{width: "100%", borderRadius: "20px"}}/>
-        </div>
+      <div style={{ display: "flex", justifyContent: "center", marginTop: 20 }}>
+        <DotLottieReact
+          src={ticketAnimation}
+          loop={true}
+          autoplay={true}
+          style={{ width: 300, height: 300 }}
+        />
+      </div>
 
+      <div>
+        <img
+          src={events}
+          alt="events"
+          style={{ width: "100%", borderRadius: "20px" }}
+        />
+      </div>
     </>
-    )
+  );
 }
