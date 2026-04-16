67fc4872615cea43b86ebae9fd1c1ef30fe7e8f0
Liovic031 <101591150+Liovic031@users.noreply.github.com>
2026-04-16 16:17:36 +0200

    Navbar hamburger izbornik

--- src/components/Izbornik.jsx
+++ src/components/Izbornik.jsx
@@ -1,23 +1,31 @@
 import { Container, Nav, Navbar } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
 import { IME_APLIKACIJE, RouteNames } from "../constants"
+
 export default function Izbornik() {
 
     const navigate = useNavigate()
 
     return (
-
-        <Navbar bg="dark" data-bs-theme="dark">
+        <Navbar bg="dark" data-bs-theme="dark" expand="lg">
             <Container>
-                <Navbar.Brand onClick={() => navigate(RouteNames.HOME)}>{IME_APLIKACIJE}</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link onClick={() => navigate(RouteNames.HOME)}>Home</Nav.Link>
-                    <Nav.Link onClick={() => navigate(RouteNames.DOGADJAJI)}>Događaji</Nav.Link>
-                    <Nav.Link onClick={() => navigate(RouteNames.KORISNICI)}>Korisnici</Nav.Link>
-                    <Nav.Link onClick={() => navigate(RouteNames.REZERVACIJE)}>Rezervacije</Nav.Link>
-                </Nav>
+
+                <Navbar.Brand onClick={() => navigate(RouteNames.HOME)}>
+                    {IME_APLIKACIJE}
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link onClick={() => navigate(RouteNames.HOME)}>Home</Nav.Link>
+                        <Nav.Link onClick={() => navigate(RouteNames.DOGADJAJI)}>Događaji</Nav.Link>
+                        <Nav.Link onClick={() => navigate(RouteNames.KORISNICI)}>Korisnici</Nav.Link>
+                        <Nav.Link onClick={() => navigate(RouteNames.REZERVACIJE)}>Rezervacije</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+
             </Container>
         </Navbar>
-
     )
 }
